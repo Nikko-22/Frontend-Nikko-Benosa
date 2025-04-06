527a40c40dd078563cc06535c5ea5f703bcc1a82
Nikko <123149713+Nikko-22@users.noreply.github.com>
2025-04-06 11:23:46 +0800

    Update intro.js

--- javascript/intro.js
+++ javascript/intro.js
@@ -1,24 +1,24 @@
-// const intro = document.getElementById("intro");
-// const body = document.body;
+const intro = document.getElementById("intro");
+const body = document.body;
 
-// const hasSeenIntro = localStorage.getItem("seenIntro");
+const hasSeenIntro = localStorage.getItem("seenIntro");
 
-// if (hasSeenIntro) {
-//   intro.style.display = "none";
-// } else {
-//   body.classList.add("noscroll"); // Disable scrolling
+if (hasSeenIntro) {
+  intro.style.display = "none";
+} else {
+  body.classList.add("noscroll"); // Disable scrolling
 
-//   setTimeout(() => {
-//     intro.classList.add("fade-out");
-//     setTimeout(() => {
-//       intro.style.display = "none";
-//       body.classList.remove("noscroll"); // Re-enable scrolling
-//       localStorage.setItem("seenIntro", true);
-//     }, 1000);
-//   }, 5500);
-// }
-// const typingSound = document.getElementById("typingSound");
+  setTimeout(() => {
+    intro.classList.add("fade-out");
+    setTimeout(() => {
+      intro.style.display = "none";
+      body.classList.remove("noscroll"); // Re-enable scrolling
+      localStorage.setItem("seenIntro", true);
+    }, 1000);
+  }, 5500);
+}
+const typingSound = document.getElementById("typingSound");
 
-// document.addEventListener("animationiteration", function () {
-//   typingSound.play();
-// });
+document.addEventListener("animationiteration", function () {
+  typingSound.play();
+});
